35508ed22ae245a81223ead5efe2b25377acb4a5
im_zephyrus <97819866+franciskendrick@users.noreply.github.com>
2022-04-05 16:58:27 +0800

    added Windows' Game's Player Gauge module

---
+++ windows/game/player_gauge.py
@@ -0,0 +1,19 @@
+import pygame
+import os
+
+pygame.init()
+path = os.path.dirname(os.path.realpath(__file__))
+
+
+class PlayerGauge:
+    # Initialize -------------------------------------------------- #
+    def __init__(self):
+        pass
+
+    # Draw -------------------------------------------------------- #
+    def draw(self, display):
+        pass
+
+    # Update ------------------------------------------------------ #
+    def update(self):
+        pass

5b47970831b4099dd9f7df8da07e91b63fe77887
im_zephyrus <97819866+franciskendrick@users.noreply.github.com>
2022-04-05 17:04:36 +0800

    added Player Gauge's image initialization

--- windows/game/player_gauge.py
+++ windows/game/player_gauge.py
@@ -1,3 +1,4 @@
+from functions import separate_sets_from_yaxis, clip_set_to_list_on_xaxis
 import pygame
 import os
 
@@ -9,6 +10,18 @@ class PlayerGauge:
     # Initialize -------------------------------------------------- #
     def __init__(self):
         pass
+
+    def init_images(self):
+        spriteset = pygame.image.load(
+            f"{path}/assets/player_gauge.png")
+        order = ["bar", "icon", "on_gauge", "off_gauge"]
+
+        # Images
+        self.images = {}
+        separated_sets = separate_sets_from_yaxis(spriteset, (255, 0, 0))
+        for name, separated_set in zip(order, separated_sets):
+            image = clip_set_to_list_on_xaxis(separated_set)
+            self.images[name] = image
 
     # Draw -------------------------------------------------------- #
     def draw(self, display):

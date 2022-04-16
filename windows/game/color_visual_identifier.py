1b91c40b664b43d6130c51f6c348b47c213478e4
im_zephyrus <97819866+franciskendrick@users.noreply.github.com>
2022-04-16 18:31:51 +0800

    added Color Identifier's images initialization

--- windows/game/color_visual_identifier.py
+++ windows/game/color_visual_identifier.py
@@ -8,7 +8,18 @@ path = os.path.dirname(os.path.realpath(__file__))
 class SpecialColorVisualIdentifier:
     # Initialize -------------------------------------------------- #
     def __init__(self):
-        pass
+        self.init_images()
+
+    def init_images(self):
+        # Images
+        self.indicators = []
+        for i in range(1, 7):
+            # Initialize
+            indicator = pygame.image.load(
+                f"{path}/assets/speical color visual indicator/indicator_{i}")
+
+            # Append
+            self.indicators.append(indicator)
 
     # Draw -------------------------------------------------------- #
     def draw(self, display):

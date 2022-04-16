1d4356f31b7074fd1409b2bcb79771c96b5e9666
im_zephyrus <97819866+franciskendrick@users.noreply.github.com>
2022-04-16 18:25:29 +0800

    added Special Color Visual Identifier module

---
+++ windows/game/color_visual_identifier.py
@@ -0,0 +1,15 @@
+import pygame
+import os
+
+pygame.init()
+path = os.path.dirname(os.path.realpath(__file__))
+
+
+class SpecialColorVisualIdentifier:
+    # Initialize -------------------------------------------------- #
+    def __init__(self):
+        pass
+
+    # Draw -------------------------------------------------------- #
+    def draw(self, display):
+        pass

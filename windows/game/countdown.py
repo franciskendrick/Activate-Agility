5c7085e22b2c476b762abd8cfc7190c5b493ee93
im_zephyrus <97819866+franciskendrick@users.noreply.github.com>
2022-04-18 12:03:17 +0800

    added Countdown's image initialization

--- windows/game/countdown.py
+++ windows/game/countdown.py
@@ -1,12 +1,27 @@
+from functions import separate_sets_from_yaxis, clip_set_to_list_on_xaxis
 import pygame
+import os
 
 pygame.init()
+path = os.path.dirname(os.path.realpath(__file__))
 
 
 class Countdown:
     # Initialize -------------------------------------------------- #
     def __init__(self):
-        pass
+        self.init_images()
+
+    def init_images(self):
+        spriteset = pygame.image.load(
+            f"{path}/assets/countdown.png")
+        order = ["title", "numbers"]
+
+        # Images
+        self.images = {}
+        separated_sets = separate_sets_from_yaxis(spriteset, (255, 0, 0))
+        for name, separated_set in zip(order, separated_sets):
+            image = clip_set_to_list_on_xaxis(separated_set)
+            self.images[name] = image   
 
     # Draw -------------------------------------------------------- #
     def draw(self, display):

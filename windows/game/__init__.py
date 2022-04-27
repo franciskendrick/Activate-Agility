b972d8bd08c07753a3c92e8d02b8e846fbf88e78
im_zephyrus <97819866+franciskendrick@users.noreply.github.com>
2022-04-27 17:33:38 +0800

    added Game's Score module

--- windows/game/__init__.py
+++ windows/game/__init__.py
@@ -2,3 +2,4 @@ from .tiles import Tiles
 from .player_gauge import PlayerGauge
 from .color_visual_identifier import SpecialColorVisualIdentifier
 from .countdown import Countdown
+from .score import HighScore, Score
---
+++ windows/game/score.py
@@ -0,0 +1,25 @@
+import pygame
+import os
+
+pygame.init()
+path = os.path.dirname(os.path.realpath(__file__))
+
+
+class HighScore:
+    # Initialize -------------------------------------------------- #
+    def __init__(self):
+        pass
+
+    # Draw -------------------------------------------------------- #
+    def draw(self, display):
+        pass
+    
+
+class Score:
+    # Initialize -------------------------------------------------- #
+    def __init__(self):
+        pass
+
+    # Draw -------------------------------------------------------- #
+    def draw(self, display):
+        pass

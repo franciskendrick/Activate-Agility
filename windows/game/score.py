3957e49e0012687d0dca3c287e86f99cba8b164a
im_zephyrus <97819866+franciskendrick@users.noreply.github.com>
2022-04-29 18:17:43 +0800

    added Score's image and rect init and pos in json

--- windows/game/score.py
+++ windows/game/score.py
@@ -1,10 +1,16 @@
 from functions import clip_set_to_list_on_xaxis
 import pygame
+import json
 import os
 
 pygame.init()
 path = os.path.dirname(os.path.realpath(__file__))
 
+# Json
+with open(f"{path}/data/game.json") as json_file:
+    game_data = json.load(json_file)
+
+# Titles
 titles = pygame.image.load(
     f"{path}/assets/score_titles.png")
 score_title, highscore_title = clip_set_to_list_on_xaxis(titles)
@@ -13,7 +19,19 @@ score_title, highscore_title = clip_set_to_list_on_xaxis(titles)
 class Score:
     # Initialize -------------------------------------------------- #
     def __init__(self):
-        pass
+        self.init_title()
+
+    def init_title(self):
+        # Initialize
+        wd, ht = score_title.get_rect().size
+        resized_image = pygame.transform.scale(
+            score_title, (wd * 2, ht * 2))
+        rect = pygame.Rect(
+            game_data["countdown_position"]["title"], 
+            resized_image.get_rect().size)
+
+        # Append
+        self.title = [resized_image, rect]
 
     # Draw -------------------------------------------------------- #
     def draw(self, display):

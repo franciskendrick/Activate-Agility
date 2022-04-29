e338b9de38afee915a2b2ff5d5d66d86e6f7fc51
im_zephyrus <97819866+franciskendrick@users.noreply.github.com>
2022-04-29 18:20:18 +0800

    added Score's number position initialization

--- windows/game/score.py
+++ windows/game/score.py
@@ -20,6 +20,7 @@ class Score:
     # Initialize -------------------------------------------------- #
     def __init__(self):
         self.init_title()
+        self.init_numbers()
 
     def init_title(self):
         # Initialize
@@ -27,11 +28,14 @@ class Score:
         resized_image = pygame.transform.scale(
             score_title, (wd * 2, ht * 2))
         rect = pygame.Rect(
-            game_data["countdown_position"]["title"], 
+            game_data["score"]["title"], 
             resized_image.get_rect().size)
 
         # Append
         self.title = [resized_image, rect]
+
+    def init_numbers(self):
+        self.number_position = game_data["score"]["numbers"]
 
     # Draw -------------------------------------------------------- #
     def draw(self, display):

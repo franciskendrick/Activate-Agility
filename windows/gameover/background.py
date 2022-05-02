0b1133b3af724efe1e5857937e55086298208eba
im_zephyrus <97819866+franciskendrick@users.noreply.github.com>
2022-05-02 17:53:08 +0800

    added Gameover's Background file

---
+++ windows/gameover/background.py
@@ -0,0 +1,13 @@
+import pygame
+import os
+
+pygame.init()
+path = os.path.dirname(os.path.realpath(__file__))
+
+
+class Window:
+    def __init__(self):
+        pass
+
+    def draw(self, display):
+        pass 

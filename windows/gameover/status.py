82b462af87eb1d3d4125f7739ac5d0f19ccd32b5
im_zephyrus <97819866+franciskendrick@users.noreply.github.com>
2022-05-04 10:32:44 +0800

    added GameOver's Status initialization

--- windows/gameover/status.py
+++ windows/gameover/status.py
@@ -1,13 +1,24 @@
 import pygame
+import json
 import os
 
 pygame.init()
 path = os.path.dirname(os.path.realpath(__file__))
 
+# Json
+with open(f"{path}/data/gameover.json") as json_file:
+    gameover_data = json.load(json_file)
+
 
 class Status:
     def __init__(self):
-        pass
+        img = pygame.image.load(
+            f"{path}/assets/status.png")
+        rect = pygame.Rect(
+            gameover_data["status_position"],
+            img.get_rect().size)
+
+        self.status = [img, rect]
 
     def draw(self, display):
         pass

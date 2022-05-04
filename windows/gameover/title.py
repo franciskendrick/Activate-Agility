a6b1af4eab19484476633b97914be769e8fae1da
im_zephyrus <97819866+franciskendrick@users.noreply.github.com>
2022-05-04 09:16:45 +0800

    added GameOver's Title initialization

--- windows/gameover/title.py
+++ windows/gameover/title.py
@@ -1,13 +1,37 @@
+from functions import clip_set_to_list_on_yaxis
 import pygame
+import json
 import os
 
 pygame.init()
 path = os.path.dirname(os.path.realpath(__file__))
 
+# Json
+with open(f"{path}/data/menu.json") as json_file:
+    gameover_data = json.load(json_file)
+
 
 class Title:
     def __init__(self):
-        pass
+        animation_set = pygame.image.load(
+            f"{path}/assets/title_animation.png")
+        self.idx = 0
+
+        self.frames = []
+        for img in clip_set_to_list_on_yaxis(animation_set):
+            # Resize Image
+            wd, ht = img.get_size()
+            size = (wd * 2, ht * 2)
+            img = pygame.transform.scale(img, size)
+
+            # Initialize Rectangle
+            rect = pygame.Rect(
+                gameover_data["title_position"], 
+                img.get_rect().size)
+
+            # Append
+            slide = [img, rect]
+            self.frames.append(slide)
 
     def draw(self):
         pass

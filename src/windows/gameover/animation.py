5817d66c94f767a2da9067cf58f13e9e7935b92f
im_zephyrus <97819866+franciskendrick@users.noreply.github.com>
2022-05-19 14:26:53 +0800

    Added gameover's animation dropdown initialization (#15)

--- src/windows/gameover/animation.py
+++ src/windows/gameover/animation.py
@@ -22,6 +22,17 @@ class Animation(NumberFont):
     def __init__(self):
         super().__init__()
 
+        # DropDown 
+        self.dropdown_image = pygame.image.load(
+            f"{resources_path}/drop_down.png")
+        self.dropdown_positions = []
+        for pos in gameover_data["dropdown_positions"]["background"]:
+            position = None if pos == [None, None] else pos
+            self.dropdown_positions.append(position)
+
+        # Index
+        self.idx = 0
+
     # Draw
     def draw(self, display):
         pass

01c906bf63d581dc4fa933714028ff97e0ec270e
im_zephyrus <97819866+franciskendrick@users.noreply.github.com>
2022-05-20 17:08:13 +0800

    Added pause's background

--- src/windows/paused/paused.py
+++ src/windows/paused/paused.py
@@ -1,3 +1,5 @@
+from windows.windows import window
+from .background import Background
 import pygame
 
 pygame.init()
@@ -6,7 +8,14 @@ pygame.init()
 class Paused:
     # Initialize -------------------------------------------------- #
     def __init__(self):
-        pass
+        wd, ht = window.rect.size
+        self.display = pygame.Surface(
+            (wd // 3, ht // 3), pygame.SRCALPHA)
+        self.display.convert_alpha()
+        self.rect = pygame.Rect((
+            0, 0), self.display.get_size())
+
+        self.background = Background()
 
     # Draw -------------------------------------------------------- #
     def draw(self, display):

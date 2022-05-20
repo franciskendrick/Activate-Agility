a01c3e085ea0c5afb5535199c4477a96a0c0d08d
im_zephyrus <97819866+franciskendrick@users.noreply.github.com>
2022-05-20 14:44:03 +0800

    Added window's subpackage, Paused
    - Added window's subpackage, Paused
    - Updated paused resources' name

---
+++ src/windows/paused/__init__.py
@@ -0,0 +1,1 @@
+from .paused import Paused
---
+++ src/windows/paused/paused.py
@@ -0,0 +1,13 @@
+import pygame
+
+pygame.init()
+
+
+class Paused:
+    # Initialize -------------------------------------------------- #
+    def __init__(self):
+        pass
+
+    # Draw -------------------------------------------------------- #
+    def draw(self, display):
+        pass

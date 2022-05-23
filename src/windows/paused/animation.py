8ead816fb0257a02bb6584bdd48ecda1aa0ce468
im_zephyrus <97819866+franciskendrick@users.noreply.github.com>
2022-05-23 15:47:35 +0800

    Added pause's dropdown initialization

--- src/windows/paused/animation.py
+++ src/windows/paused/animation.py
@@ -19,7 +19,25 @@ with open(f"{resources_path}/paused.json") as json_file:
 class Animation:
     # Initialize -------------------------------------------------- #
     def __init__(self):
-        pass
+        self.init_dropdown()
+
+        self.idx = 0
+        self.update = True
+        self.frame_limit = 7
+
+    def init_dropdown(self):
+        # DropDown
+        self.dropdown_image = pygame.image.load(
+            f"{resources_path}/drop_down.png")
+
+        # DropDown Positions
+        self.dropdown_positions = []
+        for pos in paused_data["dropdown_positions"]["background"]:
+            # Get Position
+            position = None if pos == [None, None] else pos
+
+            # Append
+            self.dropdown_positions.append(position)
 
     # Draw -------------------------------------------------------- #
     def draw(self, display):

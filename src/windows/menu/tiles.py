b37471e0e9539aa581ed8dda3b8ba749626088cb
im_zephyrus <97819866+franciskendrick@users.noreply.github.com>
2022-05-29 14:24:33 +0800

    Added menu's tiles initialization
    - Added menu's tiles initialization
    - Updated menu's tiles animation 1 file names

--- src/windows/menu/tiles.py
+++ src/windows/menu/tiles.py
@@ -19,7 +19,24 @@ with open(f"{resources_path}/menu.json") as json_file:
 class Tiles:
     # Initialize -------------------------------------------------- #
     def __init__(self):
-        pass
+        self.animations = []
+        
+        # Get Animation Folders List
+        animation_folders = os.listdir(
+            f"{resources_path}/tiles")
+
+        # Loop through Animation Folders
+        for animation_folder in animation_folders:
+            # Initialize an Animation List
+            animation = []
+
+            # Get Frames from Animation Folder
+            frames = os.listdir(
+                f"{resources_path}/tiles/{animation_folder}")
+            
+            # Append Frames to Animations
+            animation.append(frames)
+            self.animations.append(animation)
 
     # Draw -------------------------------------------------------- #
     def draw(self, display):

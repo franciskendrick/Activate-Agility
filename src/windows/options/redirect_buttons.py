53d4c2c51285341aa5fea3eb4132108216efc921
im_zephyrus <97819866+franciskendrick@users.noreply.github.com>
2022-06-04 16:52:19 +0800

    Added options' redirect buttons initialization
    - Added options' redirect buttons initialization
    - Added options' redirect buttons spriteset
    - Added options' redirect buttons position in options' JSON

--- src/windows/options/redirect_buttons.py
+++ src/windows/options/redirect_buttons.py
@@ -1,3 +1,5 @@
+from functions import clip_set_to_list_on_xaxis, palette_swap
+from windows.windows import window
 import pygame
 import json
 import os
@@ -19,7 +21,45 @@ with open(f"{resources_path}/options.json") as json_file:
 class RedirectButtons:
     # Initialize -------------------------------------------------- #
     def __init__(self):
-        pass
+        spriteset = pygame.image.load(
+            f"{resources_path}/redirect_buttons.png")
+        order = ["play", "menu"]
+        images = clip_set_to_list_on_xaxis(spriteset)
+        enlarge = 3 * window.enlarge
+
+        # Palette
+        hover_palette = {
+            (232, 193, 112): (231, 213, 179),
+            (222, 158, 65): (232, 193, 112),
+            (190, 119, 43): (222, 158, 65),
+            (32, 46, 55): (57, 74, 80),
+            (21, 29, 40): (32, 46, 55),
+            (9, 10, 20): (16, 20, 31),
+            (162, 62, 140): (198, 81, 151),
+            (122, 54, 123): (162, 62, 140)}
+
+        # Buttons
+        self.buttons = {}
+        for name, img in zip(order, images):
+            # Intialize
+            hover_img = palette_swap(
+                img.convert(), hover_palette)
+            rect = pygame.Rect(
+                options_data["buttons_position"][name],
+                img.get_rect().size)
+            hitbox = pygame.Rect(
+                rect.x * enlarge, rect.y * enlarge,
+                rect.width * enlarge, rect.height * enlarge)
+
+            # Append
+            button = [
+                False,  # is_hovered
+                img,  # orig image
+                hover_img,  # hover image
+                rect,  # rectangle
+                hitbox  # hitbox
+            ]
+            self.buttons[name] = button
 
     # Draw -------------------------------------------------------- #
     def draw(self, display):

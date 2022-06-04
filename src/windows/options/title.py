432d526af91bbdee129b6459208a6fc87141bb2a
im_zephyrus <97819866+franciskendrick@users.noreply.github.com>
2022-06-04 15:19:43 +0800

    Added options' title
    - Added options' title spriteset
    - Added options' title position in options' JSON file
    - Added options' title initialization
    - Added options' title draw function

--- src/windows/options/title.py
+++ src/windows/options/title.py
@@ -1,3 +1,4 @@
+from functions import clip_set_to_list_on_yaxis
 import pygame
 import json
 import os
@@ -18,7 +19,34 @@ with open(f"{resources_path}/options.json") as json_file:
 
 class Title:
     def __init__(self):
-        pass
+        animation_set = pygame.image.load(
+            f"{resources_path}/title_animation.png")
+        self.idx = 0
+
+        self.frames = []
+        for img in clip_set_to_list_on_yaxis(animation_set):
+            # Resize Image
+            wd, ht = img.get_size()
+            size = (wd * 2, ht * 2)
+            img = pygame.transform.scale(img, size)
+
+            # Initialize Rectangle
+            rect = pygame.Rect(
+                options_data["title_position"], 
+                img.get_rect().size)
+
+            # Append
+            frame = [img, rect]
+            self.frames.append(frame)
 
     def draw(self, display):
-        pass
+        # Reset
+        if self.idx >= len(self.frames) * 5:
+            self.idx = 0
+
+        # Draw
+        img, rect = self.frames[self.idx // 5]
+        display.blit(img, rect)
+
+        # Update
+        self.idx += 1

3fa5857fd2a7de2b91b752cd47456c8bb5c14491
im_zephyrus <97819866+franciskendrick@users.noreply.github.com>
2022-06-07 22:08:26 +0800

    Added windows' game module (#41)

---
+++ src/windows/game/game.py
@@ -0,0 +1,20 @@
+import pygame
+from .tiles import Tiles
+from .player_gauge import PlayerGauge
+from .color_visual_identifier import SpecialColorVisualIdentifier
+from .countdown import Countdown
+from .score import Score
+from .high_score import HighScore
+
+pygame.init()
+
+
+class Game:
+    def __init__(self):
+        pass
+
+    def draw(self, display):
+        pass
+
+    def update(self):
+        pass

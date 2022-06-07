744c7e29271e9744f0cd1272f9a41bca80b5d926
im_zephyrus <97819866+franciskendrick@users.noreply.github.com>
2022-06-07 15:23:59 +0800

    Added options' dropdown animation initialization
    - Added options' dropdown animation initialization
    - Added options' dropdown image
    - Added options' dropdown animation positions in options' JSON

--- src/windows/options/animation.py
+++ src/windows/options/animation.py
@@ -19,8 +19,50 @@ with open(f"{resources_path}/options.json") as json_file:
 class Animation:
     # Initialize -------------------------------------------------- #
     def __init__(self):
-        pass
+        self.init_dropdown()
+        self.init_toggleablebuttons()
 
+    def init_dropdown(self):
+        # DropDown
+        self.dropdown_image = pygame.image.load(
+            f"{resources_path}/drop_down.png")
+
+        # DropDown Positions
+        self.dropdown_positions = []
+        for pos in options_data["dropdown_positions"]["background"]:
+            # Get Position
+            position = None if pos == [None, None] else pos
+
+            # Append
+            self.dropdown_positions.append(position)
+
+    def init_toggleablebuttons(self, toggleable_buttons):
+        # Toggleable Buttons Images
+        self.toggleable_btns_images = {}
+        for (name, button) in toggleable_buttons.items():
+            # Get Toggleable Button Image
+            is_hovered, toggle_status, palette_swapped_imgs, *_ = button
+            toggle_imgs = palette_swapped_imgs["on"] if toggle_status else palette_swapped_imgs["off"]
+            img = toggle_imgs["hover"] if is_hovered else toggle_imgs["up"]
+
+            # Append Image
+            self.toggleable_btns_images[name] = img
+
+        # Toggleable Buttons Positions
+        self.toggleable_btns_positions = {}
+        data_positions = options_data["dropdown_positions"]["toggleable_buttons"]
+        for (name, positions) in data_positions.items():
+            # Append Item in Status Positions Dictionary
+            self.toggleable_btns_positions[name] = []
+
+            # Toggleable Button Position
+            for pos in positions:
+                # Get Position
+                position = None if pos == [None, None] else pos 
+
+                # Append
+                self.toggleable_btns_positions[name].append(position)
+        
     # Draw -------------------------------------------------------- #
     def draw(self, display):
         pass

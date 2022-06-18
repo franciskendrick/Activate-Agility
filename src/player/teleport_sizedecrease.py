ffad87a81473842be2332d93853b29b4c73b8eff
im_zephyrus <97819866+franciskendrick@users.noreply.github.com>
2022-06-18 16:17:17 +0800

    Added player's teleport size decrease initialization
    - Added player's size decreasing spriteset
    - Added player's teleport size decrease initialization

--- src/player/teleport_sizedecrease.py
+++ src/player/teleport_sizedecrease.py
@@ -1,3 +1,4 @@
+from functions import clip_set_to_list_on_xaxis, separate_sets_from_yaxis
 import pygame
 import json
 import os
@@ -18,7 +19,20 @@ with open(f"{resources_path}/player.json") as json_file:
 
 class TeleportSizeDecrease:
     def __init__(self):
-        pass
+        # Get Size Decreasing Images
+        spriteset = pygame.image.load(
+            f"{resources_path}/player_sizedecreasing.png")
+
+        # Separate Size Decreasing Spriteset to their Directions
+        separated_spriteset = separate_sets_from_yaxis(
+            spriteset, (255, 0, 0))
+
+        # Put Size Decreasing Images into a Dictionary
+        self.images = {
+            name: clip_set_to_list_on_xaxis(spriteset) 
+                for name, spriteset in zip(
+                    player_data["direction_order"], separated_spriteset)
+            }
 
     def draw(self, display):
         pass

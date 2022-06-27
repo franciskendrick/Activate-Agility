3c7451403f4bdb4afc5ecc183443624cf6f3e643
im_zephyrus <97819866+franciskendrick@users.noreply.github.com>
2022-06-27 17:21:12 +0800

    Added countdown's audio init & play functions

--- src/audio/countdown.py
+++ src/audio/countdown.py
@@ -14,7 +14,22 @@ resources_path = os.path.abspath(
 class CountdownAudio:
     # Initialize -------------------------------------------------- #
     def __init__(self):
-        pass
+        self.one_audio = pygame.mixer.Sound(
+            f"{resources_path}/naturalreaders_1.mp3")
+        self.two_audio = pygame.mixer.Sound(
+            f"{resources_path}/naturalreaders_2.mp3")
+        self.three_audio = pygame.mixer.Sound(
+            f"{resources_path}/naturalreaders_3.mp3")
+    
+    # Play -------------------------------------------------------- #
+    def play_one(self):
+        self.one_audio.play()
+
+    def play_two(self):
+        self.two_audio.play()
+
+    def play_three(self):
+        self.three_audio.play()
 
     # Update ------------------------------------------------------ #
     def update(self):
